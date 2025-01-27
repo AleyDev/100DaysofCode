3e218fb598f32c3262353e8278a0cdf5d6d06abc
Aleyna Kaya <devaleykaya@gmail.com>
2025-01-27 23:10:19 +0300

    file added

---
+++ Day18/Turtle-Challenge1/main.py
@@ -0,0 +1,20 @@
+# Turtle Challenge 1 - Draw a Square
+from turtle import Turtle, Screen
+
+# aley_the_turtle = Turtle()
+# aley_the_turtle.shape("turtle")
+# aley_the_turtle.color("blue")
+# aley_the_turtle.forward(100)
+# aley_the_turtle.right(90)
+
+kaya = Turtle()
+kaya.color("red")
+
+kaya.forward(100)
+kaya.left(90)
+kaya.forward(100)
+kaya.left(90)
+kaya.forward(100)
+kaya.left(90)
+kaya.forward(100)
+kaya.left(90)
