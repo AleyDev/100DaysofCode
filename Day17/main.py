fef8817cecd1fd7d9465d1c5938a2c697f09f17e
Aleyna Kaya <devaleykaya@gmail.com>
2025-01-27 22:11:46 +0300

    main file updated

--- Day17/main.py
+++ Day17/main.py
@@ -17,6 +17,6 @@ else:
 qb = QuizBrain(question_bank)
 # repeat while there are still questions remaining
 while qb.still_has_questions():
-    qb.next_question()
+    qb.next_question() 
 # no need for a + 1 to the question_number here, as it gets increased after the last question is completed
 print(f"Your final score is: {qb.score}/{qb.question_number}.")
